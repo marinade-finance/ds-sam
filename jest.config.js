aeea5d161ca2c6ef57571e9e71099d46e6591fbf
Ondra Chaloupka <chalda@marinade.finance>
2024-12-17 10:54:20 +0100

    [tests] regenerating snapshots for tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/test/**.test.ts'],
+  testMatch: ['<rootDir>/test/**.test.ts', '<rootDir>/packages/**/test/**.test.ts'],
 };
