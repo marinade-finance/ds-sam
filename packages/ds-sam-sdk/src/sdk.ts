fd196509aca8cedf8b25c2d382c364ef34a2f2ed
Ondrej Vostal <ondrej@marinade.finance>
2025-06-05 17:37:24 +0200

    Re-enable maxStakeWanted

--- packages/ds-sam-sdk/src/sdk.ts
+++ packages/ds-sam-sdk/src/sdk.ts
@@ -40,6 +40,7 @@ export class DsSamSDK {
       marinadeValidatorStakeCapSol: marinadeTotalTvlSol * this.config.maxMarinadeTvlSharePerValidatorDec,
       spendRobustReputationMult: this.config.spendRobustReputationMult,
       minBondBalanceSol: this.config.minBondBalanceSol,
+      // if maxStakeWanted == null disable the limit
       minMaxStakeWanted: this.config.minMaxStakeWanted ?? Infinity,
     }
     this.debug.pushInfo('auction constraints', JSON.stringify(constraints))
