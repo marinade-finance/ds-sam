59f6d192c9464623b7198e0a24e62ccae953dda3
Ondrej Vostal <ondrej@marinade.finance>
2025-06-12 14:49:41 +0200

    Add revenue-analysis test in case of enabled reputation limits

--- test/revenue-analysis.test.ts
+++ test/revenue-analysis.test.ts
@@ -16,5 +16,17 @@ describe('revenue analysis', () => {
         snapshotValidatorsFilePath: path.join(__dirname, 'fixtures', '650_validators.json'),
       })).toMatchSnapshot()
     })
+
+    it('evaluates revenues correctly with reputation limits enabled', async () => {
+      const commandFactory = await CommandFactory.createWithoutRunning(CliModule, new Logger())
+      const cmd = await commandFactory.resolve(AnalyzeRevenuesCommand)
+      // to debug, find validators who have expected pmpe <> actual pmpe and see if they actually changed commission and vice versa
+      expect(await cmd.getRevenueExpectationCollection({
+        inputsCacheDirPath: path.join(__dirname, 'fixtures', 'sam-run-2', 'inputs'),
+        samResultsFixtureFilePath: path.join(__dirname, 'fixtures', 'sam-run-2', 'outputs', 'results.json'),
+        snapshotValidatorsFilePath: path.join(__dirname, 'fixtures', '796_validators.json'),
+      })).toMatchSnapshot()
+    })
+
   })
 })
